9233df071e8acdd7fad84328803af5d7614989df
Lucas Cueva <lucasmaximiliano@outlook.com>
2020-11-11 17:15:18 -0300

    update

--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -5,8 +5,8 @@ import { useParams } from 'react-router-dom';
 const getItemDetail = (id) => {
     return new Promise((res) => {
         setTimeout(() => {
-            const item = { id: 1, name: "Nebulizador A Pistón", price: 6000, img: "https://miro.medium.com/max/384/1*To2H39eauxaeYxYMtV1afQ.png" };
-            res(item);
+            const items = [{ id: 1, name: "Nebulizador A Pistón", price: 6000, img: "https://miro.medium.com/max/384/1*To2H39eauxaeYxYMtV1afQ.png" }, { id: 2, name: "Termómetro Infrarrojo", price: 7000, img: "https://miro.medium.com/max/384/1*To2H39eauxaeYxYMtV1afQ.png" }, { id: 3, name: "Oxímetro De Pulso", price: 6500, img: "https://miro.medium.com/max/384/1*To2H39eauxaeYxYMtV1afQ.png" }, { id: 4, name: "Tensiómetro con Estetoscopio", price: 3500, img: "https://miro.medium.com/max/384/1*To2H39eauxaeYxYMtV1afQ.png" }]
+            res(items.find(i => `${i.id}` === id));
         }, 2000);
     })
 };
@@ -16,6 +16,7 @@ function ItemDetailContainer () {
     const { id } = useParams();
 
     useEffect(() => {
+        console.log('to id ', id); 
         getItemDetail(id).then(
             res => {
                 setItem(res);
